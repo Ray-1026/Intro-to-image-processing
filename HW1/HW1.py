af88540f6a0d74f4dd6d2e89f68f5ce34216d3b7
R-a-y-s <0410330@gmail.com>
2023-04-21 23:12:57 +0800

    update

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('../img/test.jpg')
+img = cv2.imread('img/test.jpg')
 
 x = len(img[0])  # 600
 y = len(img)  # 360
@@ -146,4 +146,4 @@ img = bicubic_interpolation(img, y, x)
 cv2.imshow('Result', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-cv2.imwrite('../img/result.jpg', img)
+cv2.imwrite('img/result.jpg', img)
